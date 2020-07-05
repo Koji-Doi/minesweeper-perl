41c2462ea916cc4067d0e2eca9d414236c78f229
Koji Doi <doikoji@gmail.com>
2020-07-06 02:42:59 +0900

    modified:   minesweeper.pl

--- minesweeper.pl
+++ minesweeper.pl
@@ -7,6 +7,8 @@ Minesweeper in perl
 
 minesweeper.pl [width] [height] [mines]
 
+minesweeper.pl -r
+
 =head1 OPTIONS
 
 =over 4
@@ -23,7 +25,7 @@ minesweeper.pl [width] [height] [mines]
 
 When you start the game, the board appears in the upper left corner of the screen. This is a "minefield". 
 On the top, bottom, left and right of the board is a single letter that represents the coordinates of the square.
-A prompt will appear below it. 
+A prompt will appear below it.
 
 Instead of using the mouse, the player must advance through the game by typing a command after this prompt.
 
@@ -42,6 +44,9 @@ For example, if you want to specify x=5 and y=2, just enter 52.
 
 =head2 Ending
 
+When all mines are correctly marked or a cell with mine is opened, the game is ended
+and game records are displayed.
+When you win, records are updated.
 
 =cut
 
@@ -68,12 +73,13 @@ if(defined &TIOCGWINSZ){
 
 $ARGV[0]=~/^-/ and my $opt = shift @ARGV;
 my($width, $height, $mine_num, $col) = @ARGV; # map width, map height, the number of mine, color mode
+($width>$scr_col-4)   and $width  = $scr_col-4;
+($height>$scr_row-10) and $height = $scr_row-10; 
 my $recfile = ($ENV{HOME}||'.')."/minesweeper.pl.rec"; # file to save play records
-if($opt=~/^-+h/){
-  print pod2usage(-verbose => 2, -input => $FindBin::Bin . "/" . $FindBin::Script);
+if(defined $opt){
+  ($opt=~/^-+h/) and print(pod2usage(-verbose => 2, -input => $FindBin::Bin . "/" . $FindBin::Script));
+  ($opt=~/^-+r/) and show_record($width, $height, $mine_num);
   exit();
-}elsif($opt=~/^-+r/){
-  show_record($width, $height, $mine_num); exit();
 }
 
 $width     = $width    || 6;
@@ -81,26 +87,28 @@ $height    = $height   || 6;
 $mine_num  = $mine_num || 4;
 $col       = $col      || '256'; # '8', '256', '8rev', '256rev'; 'rev':board is displayed in reverse mode
 my $col_offset = int(($scr_col - $width)/2);
-srand(0);
+#srand(0);
 
+# color setting
 my %color;
+# for mine number
 map{
   $color{$_} = {
     fg => ([204,204,204], [64,64,255], [0,200,0], [255,0,0], [0,0,128], [152,0,0], [0,152,152], [0,0,0], [152,152,152])[$_],
     bg => [255,255,255]
   }
 } (0..8);
-$color{'+'} = {fg=>[0,0,0],       bg=>[255,0,0]};
-$color{'_'} = {fg=>[128,128,128], bg=>[200,200,200]};
-($color{t}{bg}, $color{t}{fg}) = ([255,255,255],[0,0,0]);
+$color{'F'} = {fg=>[0,0,0],       bg=>[255,0,0]};         # flag
+$color{'_'} = {fg=>[128,128,128], bg=>[200,200,200]};     # opened empty cells
+($color{t}{bg}, $color{t}{fg}) = ([255,255,255],[0,0,0]); # text
 
-my $hit=0;    # the number of found mines
-my $opened=0; # the number of opened cells
-my $marked=0; # the number of marked cells
+my $hit    = 0; # found mines
+my $opened = 0; # opened cells
+my $marked = 0; # marked cells
 
 # make map
 my @map;
-my @map_open;
+my @map_open; # status of each cell; 1:opened, 2:marked
 for(my $x=0; $x<=$width+1; $x++){
   $map[$x]=();
   for(my $y=0; $y<=$height+1; $y++){
@@ -111,25 +119,29 @@ for(my $x=0; $x<=$width+1; $x++){
 # set mines
 for(my $i=1; $i<=$mine_num; $i++){
   my($x,$y);
-  while(1){
+  while(1){ # set mines, without duplication
     ($x, $y) = (int(rand($width))+1, int(rand($height))+1);
     $map[$x][$y]<9 and last;
   }
+  # just after setting mine, change peripheral cells immediately
   $map[$x-1][$y-1]++; $map[$x-1][$y]++; $map[$x-1][$y+1]++;
   $map[$x][$y-1]++;   $map[$x][$y]=9;   $map[$x][$y+1]++;
   $map[$x+1][$y-1]++; $map[$x+1][$y]++; $map[$x+1][$y+1]++;
 }
 
+# ----
 # play
+# ----
 my @index = ('', '1'..'9', 'a'..'z', 'A'..'Z');
 my %index;
 map {$index{$index[$_]}=$_} 0..$#index;
 my $result; # 0:failed, 1:succeeded
 my $starttime = localtime();
+# draw board
 cls();
 put(0,         3, join('', @index[1..$width]), $color{t}{bg}, $color{t}{fg});
 put($height+4, 3, join('', @index[1..$width]), $color{t}{bg}, $color{t}{fg});
-map { put($_+2, 0,        $index[$_], $color{t}{bg}, $color{t}{fg}) } 1..$height;
+map { put($_+2, 1,        $index[$_], $color{t}{bg}, $color{t}{fg}) } 1..$height;
 map { put($_+2, $width+4, $index[$_], $color{t}{bg}, $color{t}{fg}) } 1..$height;
 
 PLAY: while(1){
@@ -138,7 +150,7 @@ PLAY: while(1){
   # draw cells
   for(my $x=1; $x<=$width; $x++){
     for(my $y=1; $y<=$height; $y++){
-      my $status = ($map_open[$x][$y]==2)?'+'
+      my $status = ($map_open[$x][$y]==2)?'F'
                   :($map_open[$x][$y]==1)?(($map[$x][$y]==0)?'_':$map[$x][$y])
                   :'.';
       put($y+2, $x+2, $status, $color{$status}{fg}, $color{$status}{bg});
@@ -182,7 +194,8 @@ PLAY: while(1){
     ($map_open[$x][$y]==2) and $map_open[$x][$y]=0;
   }else{
     if($map[$x][$y]>=9){
-      err("FAILED! GAME OVER!\n");
+      $map_open[$x][$y]=1;
+      explode($x,$y);
       $result = 0;
       last PLAY;
     }
@@ -190,16 +203,32 @@ PLAY: while(1){
   }
 }
 
-# ending
-for(my $x=1; $x<=$width; $x++){
-  for(my $y=1; $y<=$height; $y++){
-    put($y+2, $x+2, ($map[$x][$y]>=9)?'*':'.'); # $map[$x][$y]);
-  }
-}
-locate($height+6, 0);
+# update record
 my $endtime = localtime();
 my $ellapsed = $endtime-$starttime;
 my ($e_h,$e_m,$e_s) = (int($ellapsed)/3600, int($ellapsed%3600/60), $ellapsed%60);
+if($result and $e_h<=10){ # ignore records longer than 10hours as abnormal
+  open(my $fho, '>>', $recfile) or die "cannot open recored file";
+  print {$fho} join("\t", pretty_date($starttime), $ellapsed, sprintf("%02d:%02d:%02d", $e_h,$e_m,$e_s), $height, $width, "${mine_num}\n");
+  close $fho;
+}
+
+# ------
+# ending
+# ------
+my %symbol = (
+  '*' => ['*', '+', 'F'],
+  '.' => ['.', '.', 'X']
+);
+for(my $x=1; $x<=$width; $x++){
+  for(my $y=1; $y<=$height; $y++){
+    my $is_mine = ($map[$x][$y]>=9)?'*':'.';
+    ($is_mine eq '*') and color(["red"]); 
+    put($y+2, $x+2, $symbol{$is_mine}[$map_open[$x][$y]]); # $map[$x][$y]);
+    ereset();
+  }
+}
+locate($height+6, 0);
 my $lastmessage = ($result==1)?"YOU WIN!":"YOU FAILED!";
 color(["red"]);
 print "\n"; cll(); print "\n"; print_center($lastmessage);  cll();
@@ -210,30 +239,21 @@ end      : %s
 ellapsed : %d:%02d:%02d                
 
 EOD
-print "\n[V]iew records. [T]ry game again, or [Q]uit game\n";
+print_center("\n[V]iew records. [T]ry game again, or [Q]uit game\n");
 while(<STDIN>){
   /q/i and exit();
   /v/i and last; #exec("perl $0 -r");
-  /t/i and exec("perl $0 $width, $height, $mine_num, $col");
+  /t/i and exec("perl $0 $width $height $mine_num $col");
 }
 
-# update record
-if($result and $e_h<=10){ # ignore records longer than 10hours as abnormal
-  open(my $fho, '>>', $recfile) or die "cannot open recored file";
-  print {$fho} join("\t", pretty_date($starttime), $ellapsed, sprintf("%02d:%02d:%02d", $e_h,$e_m,$e_s), $height, $width, "${mine_num}\n");
-  close $fho;
-}
 show_record($height, $width, $mine_num, $ellapsed);
 
-
-### END OF MAIN
-
+### ----------------
 ### helper functions
+### ----------------
 sub open_cell{ # check cells recursively, open cell, and modify @map_open
   my($x,$y) = @_;
-  if($map_open[$x][$y]==1){
-    #already opened
-  }else{
+  if($map_open[$x][$y]!=1){
     $map_open[$x][$y]=1; # set 'open' flag;
     $opened++;
     $map[$x][$y]==0 or return();
@@ -241,7 +261,7 @@ sub open_cell{ # check cells recursively, open cell, and modify @map_open
       my $xx = $x+$i;
       ($xx<=0 or $xx>$width) and next;
       foreach my $j (-1,0,1){
-        $i==0 and $j==0 and next;
+        ($i==0 and $j==0) and next;
         my $yy = $y+$j;
         ($yy<=0 or $yy>$height) and next;
         no warnings 'recursion';
@@ -252,53 +272,51 @@ sub open_cell{ # check cells recursively, open cell, and modify @map_open
   }
 }
 
-# show record
-sub show_record{
+sub show_record{ # read record file, and display current top records
   my($h, $w, $mine, $elapsed)=@_;
   open(my $fhi, '<', $recfile) or die;
   my @rec = map {chomp; [split(/\t/, $_)]} <$fhi>;
   close $fhi;
   my @rec_sort = sort {$a->[1] <=> $b->[1]} @rec;
 
+  # records in the same condition of the last game
   if(defined $h and defined $w and defined $mine){
-    my @rec1 = grep {$_->[3]==$h and $_->[4]==$w and $_->[5]==$mine} @rec;
-    print_center("\n# Best 5 records in height=$h, width=$w, mine=$mine\n\n", $scr_col);
-    print_center( "Date                    Ellapsed\n", $scr_col);
+    my @rec1 = grep {$_->[3]==$h and $_->[4]==$w and $_->[5]==$mine} @rec_sort;
+    print_center("\n*** Best 5 records in width=$w, height=$h, mine=$mine ***\n\n");
+    print_center( "Date                    Ellapsed\n");
     foreach my $r (@rec1[0..4]){
       (defined $r) or next;
-      print_center(sprintf("%s %s\n", $r->[0], $r->[2]), $scr_col);
+      print_center(sprintf("%s %s\n", $r->[0], $r->[2]));
     }
     if(defined $elapsed){
-      (scalar @rec>1) and ($elapsed==$rec1[0][1]) and print_center("You have broken the record for the fastest time.\n");
+      (scalar @rec>1) and ($elapsed==$rec1[0][1]) and (color(["red"]), print_center("NEW RECORD\n"),  ereset());
     }
   }
 
   # view unfiltered records
-  print_center("\n# Best 5 of whole records\n\n", $scr_col);
-  print_center("Date                    Ellapsed  w  h  m\n", $scr_col);
+  print_center("\n*** Best 5 of whole records ***\n\n");
+  print_center("Date                    Ellapsed  w  h  m\n");
   foreach my $r (@rec_sort[0..4]){
-    print_center(sprintf("%s %s %2d %2d %2d\n", $r->[0], $r->[2], $r->[4], $r->[3], $r->[5]), $scr_col);
+    print_center(sprintf("%s %s %2d %2d %2d\n", $r->[0], $r->[2], $r->[4], $r->[3], $r->[5]));
   }
 }
 
-# date format
-sub pretty_date{
+sub pretty_date{ # date format
   my($t) = @_; # Time::Piece
   return(join(' ', $t->ymd(), $t->wdayname, $t->hms()));
 }
 
-# supporting escape sequence
-sub locate{
+# ----- supporting escape sequence -----
+
+sub locate{ # move cursor to the specified location
   my($x, $y, $t) = @_;
-  ($x<0) and $x=0;
-  ($y<0) and $y=0;
+  ($x<0) and $x = 0;
+  ($y<0) and $y = 0;
   printf("\e[%d;%dH%s", $x, $y, $t||'');
 }
 
 sub cls{ # clear screen
-  my($x) = $_[0] || '2';
-  #0: clear after cursor, 1:clear before cursor; 2:all
-  print "\e[${x}J";
+  print "\e[2J";
 }
 
 sub cll{ # clear line
@@ -307,52 +325,56 @@ sub cll{ # clear line
   print "\e[${x}K";
 }
 
-sub color{
-  my($color1, $color0) = @_;
+sub color{ # set text and backgroud color
+  my($color1, $color0) = @_; # should be refereces of arrays
   my %col = qw/black 0 red 1 green 2 yellow 3 blue 4 magenta 5 cyan 6 white 7/;
   my $code = (defined $color1->[2])
     ?sprintf("\e[38;2;%d;%d;%dm", @$color1)
     :sprintf("\e[3%dm", $col{$color1->[0]});
-  $code .= (defined $color0->[2])
+  (defined $color0) and $code .= (defined $color0->[2])
     ?sprintf("\e[48;2;%d;%d;%dm", @$color0)
-    :''; #sprintf("\e[4%dm", $col{$color0->[0]});
+    :sprintf("\e[4%dm", $col{$color0->[0]});
   print($code);
 }
 
-sub ereset{
+sub ereset{ # reset screen setting
   print "\e[0m";
 }
 
-sub put{
+sub put{ # put text at the specified position in the specified color
   my($x, $y, $t, $color_fg, $color_bg) = @_;
   locate($x, $y+$col_offset);
   (defined $color_fg and defined $color_bg) and color($color_fg, $color_bg);
-  print "$t\n";
-  ereset();
+  print "$t\n"; ereset();
 }
 
-sub print_center{
+sub print_center{ # print text in center
   my($txt0, $width) = @_;
-  (defined $width) or $width=$scr_col;
+  (defined $width) or $width = $scr_col;
   foreach my $txt (split("\n", $txt0)){
-    my $ind = ' ' x int(($width-length($txt))/2);
-    print($ind, "$txt\n");
+    my $offset = int(($width-length($txt))/2);
+    my $ind = ' ' x $offset;
+    print("$ind$txt\n");
   }
+}
+
+sub explode{ # effect at game ending
+  my($x, $y) = @_;
+  for my $c (qw/* X * X * X/){
+    put($y+2, $x+2, $c, ['black'], ['red']);
+    select undef, undef, undef, 0.1;
+  } 
 }
 
 sub message{
   my($t) = @_;
-  #color($color{t}{fg}, $color{t}{bg});
-  locate($height+6,0); cll();
-  print "$t\n";
+  locate($height+6, 0); cll(); print "$t\n";
 }
 
 sub err{
   my($t) = @_;
   color([255,0,0],$color{t}{bg});
-  locate($height+6,0); cll();
-  print "$t\n";
-  sleep 1;
-  ereset();
+  locate($height+6, 0); cll();
+  print "$t\n";sleep 1; ereset();
 }
 
